1f06b78a3328349f2e4f48535dcd239e1b304d09
acaztec <acrinion@aztecsoftware.com>
2025-07-24 16:46:19 -0500

    Fix CSS not loading in HTML

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: './postcss.config.js',
+  },
+  build: {
+    cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  }
 })
