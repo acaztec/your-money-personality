ca93d7dbcc68454d47f274416c09aa40fa858936
acaztec <acrinion@aztecsoftware.com>
2025-07-29 15:57:22 -0500

    Updated Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -238,6 +238,7 @@ export default function Dashboard() {
                       <p className="text-gray-700">{challenge}</p>
                     </div>
                   ))}
+                </div>
               </div>
             </div>
 
